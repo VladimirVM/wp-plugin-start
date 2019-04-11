2873f2418083b801d2bba7af02446508d1a6bea5
Vladimir <vladimir.mukhin@innovecs.com>
2019-04-11 19:05:56 +0300

    media create

--- includes/Helper.php
+++ includes/Helper.php
@@ -61,6 +61,100 @@ class Helper
 		    exit;
 		}
 	}
-	
+
+	static function crateMedia($name, $local = false, $post = [], $allow_ext = ['jpg', 'png', 'gif', 'jpeg'])
+	{
+
+		$out = null;
+
+		$wordpress_upload_dir = wp_upload_dir();
+		$i = 0; // number of tries when the file with the same name is already exists
+
+		if ($local === true) {
+			$file = [
+				'path' => $name,
+				'tmp_name' => null,
+				'name' => basename($name),
+				'size' => filesize($name),
+				'error' => 0,
+			];
+		} else {
+			$file = $_FILES[$name];
+		}
+
+		if (empty($file)) {
+			return ('File is not selected.');
+		}
+		if ($file['error']) {
+			return ($file['error']);
+		}
+		if ($file['size'] > wp_max_upload_size()) {
+			return ('It is too large than expected.');
+		}
+
+		$new_file_path = $wordpress_upload_dir['path'] . '/' . $file['name'];
+		$new_file_mime = mime_content_type($file['tmp_name'] ?? $file['path']);
+
+		$info = pathinfo($new_file_path);
+
+		if (!(is_string($allow_ext) && $allow_ext === 'all')) {
+			$allow_ext = array_flip((array)$allow_ext);
+			if (!isset($allow_ext[$info['extension']])) {
+				return 'Doesn\'t allow this extension of uploads.';
+			}
+		}
+
+		if (!in_array($new_file_mime, get_allowed_mime_types())) {
+			return ('WordPress doesn\'t allow this type of uploads.');
+		}
+		while (is_file($new_file_path)) {
+			$i++;
+			$new_file_path = $wordpress_upload_dir['path'] . '/' . $i . '_' . $file['name'];
+		}
+
+		if ($local === true) {
+			$upload = copy($file['path'], $new_file_path);
+		} else {
+			$upload = move_uploaded_file($file['tmp_name'], $new_file_path);
+		}
+
+		if ($upload) {
+
+
+			$post = $post + [
+					'guid' => $new_file_path,
+					'post_mime_type' => $new_file_mime,
+					'post_status' => 'inherit',
+					'post_title' => '{name}',
+					'post_content' => '',
+				];
+
+			$post['post_title'] = str_replace(
+				['{name}', '{ext}'],
+				[$info['filename'], $info['extension']],
+				$post['post_title']
+			);
+			$post['post_content'] = str_replace(
+				['{name}', '{ext}'],
+				[$info['filename'], $info['extension']],
+				$post['post_content']
+			);
+
+			$out = wp_insert_attachment($post, $new_file_path);
+
+			// wp_generate_attachment_metadata() won't work if you do not include this file
+			require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+			// Generate and save the attachment metas into the database
+			wp_update_attachment_metadata($out, wp_generate_attachment_metadata($out, $new_file_path));
+
+			// Show the uploaded file in browser
+//			wp_redirect( $wordpress_upload_dir['url'] . '/' . basename( $new_file_path ) );
+
+		}
+
+		return $out;
+	}
+
 
 }
